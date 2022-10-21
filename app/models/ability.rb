c894d4b7cdc98ea16785fb63e7ca9e508d0f6373
Henry <dokinruss@gmail.com>
2022-10-21 15:44:17 +0000

    ADD CANCANCAN AUTHENICATION TO ESTABLISH USER ROLE AND ACCESS CONTROL

---
+++ app/models/ability.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+    def initialize(user)
+      user ||= User.new
+  
+      if user.role == 'admin'
+        can :manage, :all
+      else
+        can %i[read create], :all
+        can %i[update destroy], Post, author_id: user.id
+        can %i[update destroy], Comment, author_id: user.id
+  
+      end
+  end
+end
+end
