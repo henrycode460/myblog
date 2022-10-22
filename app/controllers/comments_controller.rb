e63a1940744174f0f52818bfe87f6cf654247999
Henry <dokinruss@gmail.com>
2022-10-22 11:08:15 +0000

    fix reviewer recommended changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,19 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.includes(:comments).find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    @post.comments_conter -= 1
+    @post.save
+
+    respond_to do |format|
+      format.html { redirect_to(user_posts_url) }
+    end
+  end
+
+
   def comment_params
     params.require(:comment).permit(:text)[:text]
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,47 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @user = User.find_by(id: params[:user_id].to_i)
     @posts = @user.posts.includes(:comments)
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    @user = current_user
+    @user.posts_counter -= 1
+    @user.save
+
+    respond_to do |format|
+      format.html { redirect_to(user_posts_url) }
+    end
+  end
+
+
   def new
     @user = current_user
   end
 
+  def post_params
+    params
+      .require(:post)
+      .permit(:title, :text)
+      .merge(author: current_user, comments_conter: 0, likes_counter: 0)
+  end
+
   def create
-    @post = Post.new(title: params[:post][:title], text: params[:post][:text])
-    @post.author = current_user
-    if @post.valid?
-      @post.save
-      redirect_to user_posts_path
-    else
-      redirect_to new_user_post_path
+    @post = Post.new(post_params)
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post saved successfully'
+          redirect_to user_post_path(current_user, @post)
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          redirect_to new_user_post_path(current_user)
+        end
+      end
     end
   end
 
@@ -25,3 +51,6 @@ class PostsController < ApplicationController
     @comments = Comment.where(post_id: params[:id].to_i)
   end
 end
+
+
+
